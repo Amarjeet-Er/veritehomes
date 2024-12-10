4b20c7a762ba7c8acca4b5f8376b53a045100f36
Amarjeet Kumar <bidupur8544@gmail.com>
2024-12-10 19:05:52 +0530

    final design

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// Function to open modal and display clicked image
+function openModal(imagePath) {
+    var modalImage = document.getElementById('fullImage');
+    modalImage.src = imagePath;  // Update the image source with the clicked image path
+}
