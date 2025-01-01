cb75e3b2e24fb8a2f92a93cdce5634f0fdf9be58
Amarjeet Kumar <bidupur8544@gmail.com>
2025-01-01 11:08:26 +0530

    final web

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,17 @@ function openModal(imagePath) {
     modalImage.src = imagePath;
 }
 
-const texts = ["Welcome to Verite Homes", "Life Products as they should be"];
-let currentIndex = 0;
-const textElement = document.getElementById("dynamic-text");
+// const texts = ["Welcome to Verite Homes", "Life Products as they should be"];
+// let currentIndex = 0;
+// const textElement = document.getElementById("dynamic-text");
 
-// Function to update the text and reset animation
-function updateText() {
-    textElement.textContent = texts[currentIndex];
-    textElement.classList.remove('slide-animation');
-    void textElement.offsetWidth;
-    textElement.classList.add('slide-animation');
-    currentIndex = (currentIndex + 1) % texts.length;
-}
-// Change text every 10 seconds
-setInterval(updateText, 2000); // Adjust interval based on your animation timing
+// // Function to update the text and reset animation
+// function updateText() {
+//     textElement.textContent = texts[currentIndex];
+//     textElement.classList.remove('slide-animation');
+//     void textElement.offsetWidth;
+//     textElement.classList.add('slide-animation');
+//     currentIndex = (currentIndex + 1) % texts.length;
+// }
+// // Change text every 10 seconds
+// setInterval(updateText, 2000); // Adjust interval based on your animation timing
